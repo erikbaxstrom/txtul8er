f7ac6163c1e4d97fe015aed7b26e62eccf11cf15
Erik M Baxstrom <baxstrem@gmail.com>
2024-01-14 12:51:33 -0800

    clean up unused code

--- looper.h
+++ looper.h
@@ -13,7 +13,6 @@ class Looper {
       memset(_buffer, 0, sizeof(float) * _buffer_length);
       // Setup the playheads
       for(int i = 0; i < _grain_count; i++){
-        // _delaySamples[i] = (i + 1) * _buffer_length / (_grain_count + 1); 
         _grain_pos[i] = _rec_head + (i + 1) * _buffer_length / (_grain_count + 1); 
       }
     }
@@ -22,18 +21,12 @@ class Looper {
       _env_step = fmap(grain_env, _min_env_step, _max_env_step);
       
       for (uint8_t i = 0; i < _grain_count; i++){
-        // if touched and less than env_max, increment _envelope[i] by 1
         if(touched & 1 << i && _envelope[i] < _env_range){
           _envelope[i] += _env_step;
         }
-        // else if not touched and greater than zero, decrement _envelope[i] by 1
         else if (!(touched & 1 << i) && _envelope[i] > 0){
           _envelope[i] -= _env_step;
         }
-        // else {
-        //   _envelope[i] = 0;
-        // }
-      }
 
     }
 
@@ -44,35 +37,27 @@ class Looper {
       _rec_head %= _buffer_length;
 
       // Playback from the buffer
-      // float _output[_grain_count] = {0};
       _output = 0;
       for(int i = 0; i < _grain_count; i++){
-        // _grain_pos[i] = (_rec_head - _delaySamples[i]) % _buffer_length;
         _output += _buffer[_grain_pos[i]] * _envelope[i] / _env_range;
         _grain_pos[i]++;
         _grain_pos[i] %= _buffer_length;
-        // _output[i] = _buffer[_grain_pos[i]] + in;}
         }
-      // _output = _output * sqrt(_grain_count) + in;
       _output = _output * sqrt(_grain_count);
       return _output;
 
     }
 
   private:
-    // static const size_t kFadeLength = 600;
-    // static const size_t kMinLoopLength = 2 * kFadeLength;
 
     float* _buffer;
 
     size_t _buffer_length = 0;
 
-
     static const int _grain_count = 4;
     size_t _grain_pos[_grain_count] = {0}; 
     size_t _rec_head  = 0;
 
-    // uint8_t _touched = 0;
     int _envelope[_grain_count] = {0};
     static const int _min_env_step = 2;
     static const int _max_env_step = 100;
@@ -80,14 +65,8 @@ class Looper {
 
     float _env_step = _min_env_step;
 
-
     float _output = 0;
     float _mix = sqrt(_grain_count);
-    // static const uint32_t _delaySec[_grain_count] = {0};
-    
-    // static const uint32_t _kSampleRate = 48000;
-    // size_t _delaySamples[_grain_count] = {0};
-
     bool _is_empty  = true;
 };
 };
--- simple-looper.ino
+++ simple-looper.ino
@@ -2,7 +2,6 @@
 #include "simple-daisy.h"
 #include "looper.h"
 #include "Adafruit_MPR121.h"
-// #include "grainloop.h"
 
 #ifndef _BV
 #define _BV(bit) (1 << (bit)) 
@@ -12,7 +11,6 @@
 static const int mix_control_pin = A(S30);
 static const int grain_env_pin   = A(S31);
 static const int g_play_speed_pin = A(S32);
-// static const int pan_pin       = A(S33);
 
 static const float kKnobMax = 1023;
 
@@ -28,7 +26,6 @@ unsigned long gPreviousMillis = 0;
 static const uint32_t gSampleIntervalStart = 100;  //milliseconds between samples. 1000 ms = 1 s
 static const uint32_t gBufferLengthSec = 300;
 static const size_t gBufferLengthSamples = gBufferLengthSec * 1000 / gSampleIntervalStart; 
-// static uint16_t DSY_SDRAM_BSS gBuffer[gBufferLengthSamples];
 uint16_t gBuffer[gBufferLengthSamples] = {0};
 uint32_t gSampleInterval = gSampleIntervalStart; 
 
@@ -41,30 +38,19 @@ size_t gloopend = 0;
 uint16_t g_read = 0;
 
 static synthux::Looper looper;
-// static PitchShifter pitch_shifter;
 float mix_control = 0;
-// float pan_control = 0; 
-
 
 Adafruit_MPR121 cap = Adafruit_MPR121();
 
-// Keeps track of the last pins touched
-// so we know when buttons are 'released'
+// Keeps track of the last pins touched so we know when buttons are 'released'
 uint16_t lasttouched = 0;
 uint16_t currtouched = 0;
 
 
-
 void AudioCallback(float **in, float **out, size_t size) {
-  // for (size_t i = 0; i < size; i++) {
-  //   auto looper_out = looper.Process(in[1][i]);
-  //   auto shifted = pitch_shifter.Process(looper_out);
-  //   out[0][i] = out[1][i] = looper_out;
-  // }
   // mix 
   for (size_t i = 0; i < size; i++) {
     auto looper_out = looper.Process(in[1][i]);
-    // float pitch_shifter_out = pitch_shifter.Process(looper_out);
     float mix = ( mix_control * in[1][i] + (1 - mix_control) * looper_out ) * 0.707;
     out[0][i] = out[1][i] = mix;
   }
@@ -77,15 +63,6 @@ void setup() {
 
   // Setup looper
   looper.Init(buffer, kBufferLengthSamples);
-
-  // Setup grainloop
-  // memset(gBuffer, 0, sizeof(uint16_t) * gBufferLengthSamples);
-
-  // Setup pitch shifter
-  // pitch_shifter.Init(sample_rate);
-
-  // Setup pins
-  // pinMode(record_pin, INPUT_PULLUP);
 
   // Setup serial for cap touch debug
     Serial.begin(9600);
@@ -109,22 +86,9 @@ void loop() {
   // Get the currently touched pads
   currtouched = cap.touched();
 
-
-  // for (uint8_t i=0; i<12; i++) {
-  //   // it if *is* touched and *wasnt* touched before, alert!
-  //   if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
-  //     Serial.print(i); Serial.println(" touched");
-  //   }
-  //   // if it *was* touched and now *isnt*, alert!
-  //   if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
-  //     Serial.print(i); Serial.println(" released");
-  //   }
-  // }
-
   // read/set play/pause and re cord states from currtouched
   // set grain record state
   if ((currtouched & _BV(grain_rec_button)) && !(lasttouched & _BV(grain_rec_button)) ) {
-      // Serial.println(" record toggled");
       grain_is_playing = false;
       g_read = 0;
       if(grain_is_recording){ 
@@ -135,22 +99,11 @@ void loop() {
         grain_is_playing = false;  //stop playing
         grain_is_recording = true; //start recording
       }
-      // grain_is_recording = !grain_is_recording;
-      // Serial.println("rec toggled");
-      // Serial.println(grain_is_recording? "R ": "r "); 
-      // Serial.println(grain_is_playing? "P": "q");
-      // Serial.println(gBuffer[g_headposition]);
     }
   // set grain playback state
   if ((currtouched & _BV(grain_play_button)) && !(lasttouched & _BV(grain_play_button)) ) {
-      // Serial.println(" play toggled");
       grain_is_playing = !grain_is_playing;
       g_read = 0;
-      // Serial.println("play toggled");
-      // Serial.println(grain_is_recording? "R ": "r "); 
-      // Serial.println(grain_is_playing? "P": "q");
-      // Serial.println(g_headposition);
-      // Serial.println(gBuffer[g_headposition]);
     }
   if((currtouched & _BV(grain_play_button)) && (currtouched & _BV(grain_rec_button))){
     // both touched. erase grain buffer
@@ -158,9 +111,7 @@ void loop() {
     grain_is_playing = false;
     g_headposition = 0;
     gloopend = 0;
-    // Serial.println("erased buffer");
     delay(100);
-    //reset
   }
 
   lasttouched = currtouched;
@@ -169,10 +120,8 @@ void loop() {
   gCurrentMillis = millis();
   if(gCurrentMillis - gPreviousMillis >= gSampleInterval){
     gPreviousMillis = gCurrentMillis;
-    // Serial.println(gCurrentMillis);
     // if recording, write currtouched to buffer at headposition on, update loopend if needed
     if(grain_is_recording){
-      // Serial.print(gCurrentMillis); Serial.println('grain is recording');
       gBuffer[g_headposition] = currtouched & 15; //bitwise 'and' w/ 15 to mask out the play/pause buttons
       g_headposition++;
       if(g_headposition > gBufferLengthSamples){ //end of buffer. loop back to beginning of buffer. set gloopend to buffer length
@@ -185,78 +134,16 @@ void loop() {
     }
     // if playing, read from buffer at headposition, wrapping at loopend
     if(grain_is_playing){
-      // Serial.print(gCurrentMillis); Serial.println('grain is playing');
       g_read = gBuffer[g_headposition];
       g_headposition++;
       g_headposition %= gloopend;
     }
 
-    // update headposition
   }
   // 'or' together the currtouched and the current grainloop before passing currtouched to setTaps below
-  // Serial.print(currtouched);
-  // Serial.print(" ");
-  // Serial.print(g_read);
-  // currtouched = currtouched | g_read;
   currtouched = currtouched | g_read;
-  // Serial.print(" ");
-  // Serial.println(currtouched);
-
-
 
   auto grain_env = analogRead(grain_env_pin) / kKnobMax;
 
   looper.setTaps(currtouched, grain_env);
-
-  // // Deal with touch and release of pads DONT NEED
-  // for (uint8_t i=0; i<12; i++) {
-  //   // it if *is* touched and *wasnt* touched before, alert!
-  //   if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
-  //     Serial.print(i); Serial.println(" touched");
-  //   }
-  //   // if it *was* touched and now *isnt*, alert! 
-  //   if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
-  //     Serial.print(i); Serial.println(" released");
-  //   }
-  // }
-  //
-  // // reset our state
-  // lasttouched = currtouched;
-
-
-
-  // // Set loop parameters by analog reading pot values DONT NEED
-  // // auto loop_start = fmap(analogRead(loop_start_pin) / kKnobMax, 0.f, 1.f);
-  // // auto loop_length = fmap(analogRead(loop_length_pin) / kKnobMax, 0.f, 1.f, Mapping::EXP);
-  // // looper.SetLoop(loop_start, loop_length);
-  // //this is code I wrote. probably dont need this either
-  // auto loop_start = 0;
-  // auto loop_length = 1;
-  // looper.SetLoop(loop_start, loop_length);
-
-  // Toggle record from touchpad 0 DONT NEED
-  // auto record_on = digitalRead(record_pin);
-  // auto record_on = currtouched & _BV(0);
-  // if(record_on){
-  //   Serial.print('0'); Serial.println(" on");
-  // }
-  // looper.SetRecording(record_on);
-
-  // always be recording
-  // auto record_on = true;
-  // looper.SetRecording(record_on);
-
-//   // Set pitch
-//   auto pitch_val = fmap(analogRead(pitch_pin) / kKnobMax, 0.f, 1.f);
-//   set_pitch(pitch_val);
-// }
-
-// void set_pitch(float pitch_val) {
-//   int pitch = 0;
-//   // Allow some gap in the middle of the knob turn so 
-//   // it's easy to cacth zero position
-//   if (pitch_val < 0.45 || pitch_val > 0.55) {
-//     pitch = 24.0 * (pitch_val - 0.5);
-//   }
-//   pitch_shifter.SetTransposition(pitch);
 }
